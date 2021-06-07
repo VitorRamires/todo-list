f6d5003651097d9db7605c6b5a26c2b625d16e57
Vitor <vitorm.ramires@hotmail.com>
2021-06-07 10:34:31 -0300

    Continuando a estilização

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
 var inputTodo = document.getElementById('input-todo')
 var painelTarefas = document.getElementById('painel-tarefas')
-
 var buttonAdicionar = document.querySelector('.adicionar')
+var criarList = document.getElementById('lista-tarefas')
+
 buttonAdicionar.addEventListener('click',adicionarTarefa)
 
-var criarList = document.getElementById('lista-tarefas')
 
 function adicionarTarefa(){
     if(inputTodo.value !== ""){
         var criarItem = document.createElement('li')
+        var acoes = document.createElement('div')
+        acoes.setAttribute('class', 'acoes')
         criarItem.setAttribute('class','lista-item')
+        
 
         criarItem.textContent = inputTodo.value
+
+        criarItem.appendChild(acoes)
         painelTarefas.appendChild(criarList)
         criarList.appendChild(criarItem)
+
     }
     
     //botao remover
     var botaoRemover = document.createElement('button')
     botaoRemover.setAttribute('href', 'remover')
     botaoRemover.textContent = "Remover"
-    criarItem.appendChild(botaoRemover)
+    acoes.appendChild(botaoRemover)
     botaoRemover.onclick = function(){
         criarItem.removeChild(botaoRemover)
         criarList.removeChild(criarItem)
@@ -29,12 +35,15 @@ function adicionarTarefa(){
     //botao Checar
     var botaoChecar = document.createElement('input')
     botaoChecar.setAttribute('type', 'checkbox')
-    criarItem.appendChild(botaoChecar)
+    acoes.appendChild(botaoChecar)
         botaoChecar.onclick = function(){
             if(!botaoChecar.checked){
                 criarItem.style.textDecoration = "none"
             } else {
                 criarItem.style.textDecoration = "line-through"
             }
-        } 
+    } 
+
+    inputTodo.value = ""
+
 }
