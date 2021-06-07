df265dc21c6f733cf5005dfbcbb0a1d4242456a4
Vitor <vitorm.ramires@hotmail.com>
2021-06-07 19:38:00 -0300

    Criando contador e estilizando

--- script.js
+++ script.js
@@ -2,47 +2,76 @@ var inputTodo = document.getElementById('input-todo')
 var painelTarefas = document.getElementById('painel-tarefas')
 var buttonAdicionar = document.querySelector('.adicionar')
 var criarList = document.getElementById('lista-tarefas')
+var itemArray = []
+var countAdd = 0
+var countCheck = 0
+var contagemAdd = document.getElementById('ativas')
+var contagemCheck = document.getElementById('completas')
 
 buttonAdicionar.addEventListener('click',adicionarTarefa)
 
 
 function adicionarTarefa(){
+    
+    
     if(inputTodo.value !== ""){
         var criarItem = document.createElement('li')
         var acoes = document.createElement('div')
         acoes.setAttribute('class', 'acoes')
         criarItem.setAttribute('class','lista-item')
         
-
         criarItem.textContent = inputTodo.value
-
+        
         criarItem.appendChild(acoes)
         painelTarefas.appendChild(criarList)
         criarList.appendChild(criarItem)
-
+        
+        itemArray.unshift(inputTodo.value)
+        
+        countAdd ++
+        contagemAdd.textContent = countAdd
+        
+    } else {
+        alert("Adicione uma tarefa")
     }
     
     //botao remover
     var botaoRemover = document.createElement('button')
     botaoRemover.setAttribute('href', 'remover')
+    
     acoes.appendChild(botaoRemover)
     botaoRemover.onclick = function(){
         criarItem.removeChild(acoes)
         criarList.removeChild(criarItem)
+        countAdd --
+        contagemAdd.textContent = countAdd
     }
-
+    
     //botao Checar
     var botaoChecar = document.createElement('input')
     botaoChecar.setAttribute('type', 'checkbox')
+    
     acoes.appendChild(botaoChecar)
-        botaoChecar.onclick = function(){
-            if(!botaoChecar.checked){
+    botaoChecar.onclick = function(){
+        if(!botaoChecar.checked){
                 criarItem.style.textDecoration = "none"
+                criarItem.style.color = "white"
+                countCheck --
+                contagemCheck.textContent = countCheck
             } else {
                 criarItem.style.textDecoration = "line-through"
+                criarItem.style.color = "#c3c1c1"
+                countCheck ++
             }
+            contagemCheck.textContent = countCheck
     } 
 
     inputTodo.value = ""
+  
 
+
+ 
 }
+
+
+
